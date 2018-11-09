8592557e876860f745aa29fd1fd72b009b3affb3
crashnorun <charlie.portelli@gmail.com>
2018-11-08 19:38:06 -0500

    added tests

--- sketch.test.js
+++ sketch.test.js
@@ -3,7 +3,13 @@ const sum = require('./sketch');
 
 test('adds 1 + 2 to equal 3', sumTest);
 
-
 function sumTest() {
     expect(sum(1, 2)).toBe(3);
+}
+
+
+test('second test', sumTest2);
+
+function sumTest2() {
+    expect(sum(2, 2)).toBe(4);
 }
